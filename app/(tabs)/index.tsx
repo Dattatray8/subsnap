5268019028fe3fef952401c70aeccf9916bdca98
Dattatray8 <dattatraymahindrakar@gmail.com>
2026-03-31 20:00:01 +0530

    feat: project setup, nativewind, routing, tab navigation

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,13 @@
 import { Link } from "expo-router";
 import { Text, View } from "react-native";
+import {styled} from "nativewind";
+import { SafeAreaView as SAV } from "react-native-safe-area-context";
+
+const SafeAreaView = styled(SAV);
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-background">
+    <SafeAreaView className="flex-1 items-center justify-center bg-background">
       <Text className="text-xl font-bold text-success">
         Welcome to Nativewind!
       </Text>
@@ -12,6 +16,6 @@ export default function App() {
           Get Started
         </Text>
       </Link>
-    </View>
+    </SafeAreaView>
   );
 }
--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,11 +1,15 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import {styled} from "nativewind";
+import { SafeAreaView as SAV } from "react-native-safe-area-context";
+
+const SafeAreaView = styled(SAV);
 
 const settings = () => {
   return (
-    <View>
+    <SafeAreaView>
       <Text>settings</Text>
-    </View>
+    </SafeAreaView>
   )
 }
 
