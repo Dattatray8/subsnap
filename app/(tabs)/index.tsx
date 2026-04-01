52d5f261fe17962c22ffc8243de87df005ed4e62
Dattatray8 <dattatraymahindrakar@gmail.com>
2026-04-01 07:14:08 +0530

    feat: add fonts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,7 +8,7 @@ const SafeAreaView = styled(SAV);
 export default function App() {
   return (
     <SafeAreaView className="flex-1 items-center justify-center bg-background">
-      <Text className="text-xl font-bold text-success">
+      <Text className="text-xl text-success font-sans-bold">
         Welcome to Nativewind!
       </Text>
       <Link href={'/login'} className="mt-4 rounded-full bg-accent py-4 px-8">
